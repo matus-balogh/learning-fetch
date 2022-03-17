2e994ffa07f0ac3d86d14f86079b031f0b0923c0
matus-balogh <99489381+matus-balogh@users.noreply.github.com>
2022-03-17 08:47:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function fetchData() {
+    fetch('https://content.guardianapis.com/search?api-key=4755d17e-dce0-4b88-ba7c-bcc8f0e21ed9')
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR you fool");
+            }
+            return response.json();
+        }).then(data => {
+           console.log(data.response.results); 
+           const html = data.response.results.map(user => {
+               return `<p>Title: ${user.webTitle} </p>`
+           }).join('');
+           console.log(html);
+            document
+            .querySelector('#app')
+            .insertAdjacentHTML("afterbegin", html);
+
+        }).catch(error => {
+            console.log(error)
+        });
+}
+
+fetchData();
